34b730cf6ebad6a73f7a9b65ae237a4ce96be7fd
HeroGenie <liebkne.kim@gmail.com>
2017-03-29 20:12:24 +0900

    init

---
+++ HGManualLayout/HGManualLayout/HGManualLayout.h
@@ -0,0 +1,19 @@
+//
+//  HGManualLayout.h
+//  HGManualLayout
+//
+//  Created by HeroGenie on 2017. 3. 29..
+//  Copyright © 2017년 HeroGenie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HGManualLayout.
+FOUNDATION_EXPORT double HGManualLayoutVersionNumber;
+
+//! Project version string for HGManualLayout.
+FOUNDATION_EXPORT const unsigned char HGManualLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HGManualLayout/PublicHeader.h>
+
+
